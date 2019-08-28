ef79c4df8c0c91b470402ca1acb5cec7bd5e0150
Wendy Guo <xiaoqiguo0715@gmail.com>
2019-08-27 20:30:30 -0400

    convert dict to json

--- app.py
+++ app.py
@@ -11,24 +11,25 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # 0. read data from database
-    engine = create_engine(f"postgresql://postgres:postgres@localhost/nfl_db")
+    engine = create_engine(f"postgresql://postgres:postgres@localhost/nfl")
     df_player = pd.read_sql("""select * from "Player" where player_name = 'P.Manning' """, engine)
 
     df_player.fillna(0, inplace=True)
-    df_player_year = df_player.groupby(["game_year"]).sum()
+    df_player_year = df_player.drop(columns=['play_type'])
+    df_player_year = df_player_year.groupby(["player_id","player_name","game_year"]).sum()
     df_player_year = df_player_year.reset_index()
 
     player_info = {}
-    player_name = list(df_player.player_name.unique())[0]
+    player_name = list(df_player_year.player_name.unique())[0]
     player_info[player_name] = {}
 
     year_list = sorted(list(df_player_year.game_year.unique()))
     for year in year_list:
-        df_info = df_player[df_player.game_year == year][['passing_yards_gained', 'receiving_yards_gained', 'rushing_yards_gained']]
+        df_info = df_player_year[df_player_year.game_year == year][['passing_yards_gained', 'receiving_yards_gained', 'rushing_yards_gained']]
         df_info['json'] = df_info.apply(lambda x: x.to_json(), axis=1)
-
-        player_info[player_name][year] = ','.join(df_info['json'].unique())
-
+        
+    #     player_info[player_name][year] = ','.join(df_info['json'].unique())
+        player_info[player_name][year] = list(df_info['json'].unique())[0]
     player_info
 
 
@@ -36,30 +37,41 @@ def index():
 
 @app.route("/player_data", methods=['GET'])
 def pass_data():
+    # STEP 1: API Receive play_name as a value, default is 'P,Manning'
     if 'player_name' in request.args:
         player_name = request.args['player_name']
     else:
         player_name = 'P.Manning'
-    engine = create_engine(f"postgresql://postgres:postgres@localhost/nfl_db")
+    # STEP 2: Query Player Data from DB, based on the api received value    
+    engine = create_engine(f"postgresql://postgres:postgres@localhost/nfl")
     df_player = pd.read_sql(f"""select * from "Player" where player_name = '{player_name}' """, engine)
-
+    # STEP 2.2: if nothing return, return 
+    if len(df_player) == 0:
+        return json.dumps({'error_msg':'player not found!'})
+    # STEP 3: Transform Data
+    # STEP 3.1: clean data & group by 
     df_player.fillna(0, inplace=True)
-    df_player_year = df_player.groupby(["game_year"]).sum()
+    df_player_year = df_player.drop(columns=['play_type'])
+    df_player_year = df_player_year.groupby(["player_id","player_name","game_year"]).sum()
     df_player_year = df_player_year.reset_index()
-
+    # STEP 3.2: turn dataframe to dictionary
     player_info = {}
-    player_name = list(df_player.player_name.unique())[0]
+    player_name = list(df_player_year.player_name.unique())[0]
     player_info[player_name] = {}
 
     year_list = sorted(list(df_player_year.game_year.unique()))
-
+    # STEP 3.3: extract data by year
     for year in year_list:
-        df_info = df_player[df_player.game_year == year][['passing_yards_gained', 'receiving_yards_gained', 'rushing_yards_gained']]
-        df_info['json'] = df_info.apply(lambda x: x.to_json(), axis=1)
-
-        player_info[player_name][year] = ','.join(df_info['json'].unique())
-
-    return json.dumps(player_info)
+        # 3.3.1. select by year
+        df_info = df_player_year[df_player_year.game_year == year][['passing_yards_gained', 'receiving_yards_gained', 'rushing_yards_gained']]
+        # 3.3.2. transfrom each to dict
+        df_info['json'] = df_info.apply(lambda x: x.to_dict(), axis=1)
+        # 3.3.3. create key, value pair dictionary
+        player_info[player_name][year] = list(df_info['json'])[0]
+    
+    # STEP 4. Turn dictionary to json for frontend to use
+    data = json.dumps(player_info)
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
